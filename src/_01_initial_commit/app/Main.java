12865b1ea9df4d009d89bfd7c6621e8ec667b460
Andriy Svidlo <komandrei5@gmail.com>
2023-04-01 23:39:18 +0300

    Change app info: short text

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -1,13 +1,16 @@
 package _01_initial_commit.app;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("Super app!");
 
         int a = 8;
         int b = 2;
         int c = add(a, b);
         System.out.println(c);
     }
+
     private static int add(int a, int b) {
         return a + b;
     }
